46205d7a185b33b7137dedc5b98eff3e6827436d
Gennady Denisenko <gennadii.denisenko@gmail.com>
2025-04-28 21:50:00 +0300

    Mod: exercise3.cpp | Clean and format source code

--- ch3/exercises/exercise3.cpp
+++ ch3/exercises/exercise3.cpp
@@ -4,14 +4,15 @@ using namespace std;
 int main () {
     vector<double> temps;
     for(double temp; cin>>temp;) {
-
         temps.push_back(temp);
-        sort(temps.begin(), temps.end());
         cout << "\nEntered elements: " << temps.size() << endl;
+        
         cout << "Sorted sequence: ";
+        sort(temps.begin(), temps.end());
         for(double temp : temps) {
             cout << temp << '\t';
         }
+
         cout << "\nNew median temperature is: ";
         if(temps.size()%2 == 1) cout << temps[temps.size()/2] << endl;
         else cout << (temps[temps.size()/2] + (temps[temps.size()/2-1])) / 2 << endl;
