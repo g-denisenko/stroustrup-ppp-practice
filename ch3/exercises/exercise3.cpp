f8a8e41cc4cc783d04981631c93d189ccc0c6a7d
Gennady Denisenko <gennadii.denisenko@gmail.com>
2025-04-28 21:47:29 +0300

    Add: exercise3.cpp | Solution task 3 for chapter 3

---
+++ ch3/exercises/exercise3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    vector<double> temps;
+    for(double temp; cin>>temp;) {
+
+        temps.push_back(temp);
+        sort(temps.begin(), temps.end());
+        cout << "\nEntered elements: " << temps.size() << endl;
+        cout << "Sorted sequence: ";
+        for(double temp : temps) {
+            cout << temp << '\t';
+        }
+        cout << "\nNew median temperature is: ";
+        if(temps.size()%2 == 1) cout << temps[temps.size()/2] << endl;
+        else cout << (temps[temps.size()/2] + (temps[temps.size()/2-1])) / 2 << endl;
+    }
+    return 0;
+}
