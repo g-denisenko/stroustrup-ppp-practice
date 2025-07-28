a75d1f2b6ecd41256eebc27c34e49af2de00667b
Gennady Denisenko <gennadii.denisenko@gmail.com>
2025-07-28 12:08:01 +0300

    Initial condition for task commit

---
+++ ch4/exercises/exercise02.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+double ctok(double c)                 // converts Celsius to Kelvin
+{
+    int k = c + 273.25;
+    return int
+}
+
+int main()
+{
+    double c = 0;                     // declare input variable
+    cin >> d;                         // retrieve temperature to input variable
+    double k = ctok("c");             // convert temperature
+    Cout << k << '/n' ;               // print out temperature
+}
