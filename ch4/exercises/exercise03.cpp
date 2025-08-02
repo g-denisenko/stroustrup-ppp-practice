4da5e7ab34d88e5732b423416343de3e9b8cabd3
Gennady Denisenko <gennadii.denisenko@gmail.com>
2025-08-02 13:12:33 +0300

    Mod: exercise03.cpp - rewritten solution as was asked in the task

--- ch4/exercises/exercise03.cpp
+++ ch4/exercises/exercise03.cpp
@@ -1,11 +1,13 @@
-#include <cassert>
 #include <iostream>
 #include <limits>
 using namespace std;
 
+void error (string s) {
+    throw runtime_error{s};
+}
+
 double celsius_to_kelvin(double celsius)
 {
-    assert(celsius > -273.15 && "REASON: Temperature cannnot below absolute 0");
     return celsius + 273.15;
 }
 
@@ -13,12 +15,18 @@ int main()
 {
     cout << "Enter a temperature in Celsius: " << endl;
     double celsius = 0;
-    while(!(cin>>celsius)){
-        cin.clear();
-        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        cerr << "Wrong input. Try again" << endl;
+    try {
+        while(!(cin>>celsius)) {
+            cin.clear();
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            cerr << "Wrong input. Try again" << endl;
+        }
+        if(celsius < -273.15) error("Entered Celsus below -273.15");
+        cout << "You entered Celsius: " << celsius << endl;
+        double kelvin = celsius_to_kelvin(celsius);
+        cout << celsius << " celsius in kelvin is: " << kelvin << endl;
     }
-    cout << "You entered Celsius: " << celsius << endl;
-    double kelvin = celsius_to_kelvin(celsius);
-    cout << celsius << " celsius in kelvin is: " << kelvin << endl;
+    catch(runtime_error& e) {
+        cerr << "Absolute temperature cannot be below 0" << endl;
+    }
 }
