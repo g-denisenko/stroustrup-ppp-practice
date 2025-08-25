e48ab5b9f1878d31d60caf27174922f0cf03329d
Gennady Denisenko <gennadii.denisenko@gmail.com>
2025-08-25 16:35:49 +0300

    Mod: exercise12.cpp | Used vector of ints instead ints. Solution completed. Game is ready for play

--- ch4/exercises/exercise12.cpp
+++ ch4/exercises/exercise12.cpp
@@ -5,23 +5,18 @@
 
 using namespace std;
 
-bool check_digit_repetition(int n) {
-    assert(n>999 && n<10000 && "Not a 4-digit number. ERROR");
-    vector<int> digits;
-    digits.push_back((n%10000)/1000);
-    digits.push_back((n%1000)/100);
-    digits.push_back((n%100)/10);
-    digits.push_back((n%10)/1);
+bool check_digit_repetition(vector<int> n) {
+    assert(n.size()==4 && "Not a 4-digit number. ERROR");
 
-    for(int i=0; i<digits.size(); i++) {
-        for(int j=0; j<digits.size(); j++) {
-            if(i!=j && digits[i]==digits[j]) return true;
+    for(int i=0; i<n.size(); i++) {
+        for(int j=0; j<n.size(); j++) {
+            if(i!=j && n[i]==n[j]) return true;
         }
     }
     return false;
 }
 
-int user_guess() {
+vector<int> user_guess() {
     cout << "Enter an 4-digit integer: " << endl;
     int n = 0;
     while(true) {
@@ -35,18 +30,29 @@ int user_guess() {
 
         assert(n<INT_MAX && "OUT OF RANGE of maximum");
 
-        if(n<1000 || n>9999) {
+        if(n>9999) {
             cout << "Not a 4-digit integer. Please, try again." << endl;
             continue;
         }
 
-        if(check_digit_repetition(n)) {
+        if(n<1000) {
+            cout << "Not a 4-digit integer (or started from '0' -zero). Please, try again." << endl;
+            continue;
+        }
+
+        vector<int> result;
+        result.push_back((n%10000)/1000);
+        result.push_back((n%1000)/100);
+        result.push_back((n%100)/10);
+        result.push_back((n%10)/1);
+
+        if(check_digit_repetition(result)) {
             cout << "Duplicate digit was found. Please, try again." << endl;
             continue;
         }
 
-        cout << "User input confirmed as: " << n << endl;
-        return n;
+        // cout << "User input confirmed as: " << n << endl;
+        return result;
     }
 }
 
@@ -58,33 +64,87 @@ int random_int() {
     return n;
 }
 
-int secret_four_digit() {
-    int secret_four_digit = 0;
+vector<int> secret_num() {
+    vector<int> result;
     bool a, b, c, d, e, f, g, h, i, j;
     a = b = c = d = e = f = g = h = i = j = false;  // Flag for repeated digit encounter
-    int multy = 1000; 
-    for(int i=0; i<=4; i++) {
+    for(int item=0; item<4; item++) {
         int rand = random_int();
-        if(rand==0 && !a && multy!=1000) {a=true; multy /= 10; continue;}
-        if(rand==1 && !b) {b=true; secret_four_digit = secret_four_digit+multy*rand; multy /= 10; continue;}
-        if(rand==2 && !c) {c=true; secret_four_digit = secret_four_digit+multy*rand; multy /= 10; continue;}
-        if(rand==3 && !d) {d=true; secret_four_digit = secret_four_digit+multy*rand; multy /= 10; continue;}
-        if(rand==4 && !e) {e=true; secret_four_digit = secret_four_digit+multy*rand; multy /= 10; continue;}
-        if(rand==5 && !f) {f=true; secret_four_digit = secret_four_digit+multy*rand; multy /= 10; continue;}
-        if(rand==6 && !g) {g=true; secret_four_digit = secret_four_digit+multy*rand; multy /= 10; continue;}
-        if(rand==7 && !h) {h=true; secret_four_digit = secret_four_digit+multy*rand; multy /= 10; continue;}
-        if(rand==8 && !i) {i=true; secret_four_digit = secret_four_digit+multy*rand; multy /= 10; continue;}
-        if(rand==9 && !j) {j=true; secret_four_digit = secret_four_digit+multy*rand; multy /= 10; continue;}
-        i--;    // In case if no digit added for 4-digit number
+        if(rand==0 && !a && i!=0) {a=true; result.push_back(0); continue;}
+        if(rand==1 && !b) {b=true; result.push_back(1); continue;}
+        if(rand==2 && !c) {c=true; result.push_back(2); continue;}
+        if(rand==3 && !d) {d=true; result.push_back(3); continue;}
+        if(rand==4 && !e) {e=true; result.push_back(4); continue;}
+        if(rand==5 && !f) {f=true; result.push_back(5); continue;}
+        if(rand==6 && !g) {g=true; result.push_back(6); continue;}
+        if(rand==7 && !h) {h=true; result.push_back(7); continue;}
+        if(rand==8 && !i) {i=true; result.push_back(8); continue;}
+        if(rand==9 && !j) {j=true; result.push_back(9); continue;}
+        item--; // In case if no digit added for 4-digit number
     }
-    assert(!check_digit_repetition(secret_four_digit) && "Wrong secret 4-digit number. Was generated number with digit repetition.");
-    return secret_four_digit;
+    assert(!check_digit_repetition(result) && "Wrong secret 4-digit number. Was generated number with digit repetition.");
+    return result;
+}
+
+void print_vector_ints(vector<int> v) {
+    for(int i=0; i<v.size(); i++) {
+        cout << v[i];
+    }
+}
+
+int how_many_cows(vector<int> guess, vector<int> secret) {
+    int cows = 0;
+    for(int i=0; i<secret.size(); i++) {
+        for(int j=0; j<guess.size(); j++) {
+            if(guess[j]==secret[i] && guess[j]!=secret[j]) cows++;
+        }
+    }
+    return cows;
+}
+
+int how_many_bulls(vector<int> guess, vector<int> secret) {
+    int bulls = 0;
+    for(int i=0; i<secret.size(); i++) {
+        if(guess[i]==secret[i]) bulls++;
+    }
+    return bulls;
+}
+
+bool game_cycle(vector<int> guess, vector<int> secret, int cycle_counter) {
+    int bulls = how_many_bulls(guess, secret);
+    int cows = how_many_cows(guess, secret);
+    if(bulls == 4) return false;
+    cout << "\nCycle #" << cycle_counter << endl;
+    cout << "User guess: ";
+    print_vector_ints(guess);
+    cout << endl;
+    cout << "Bulls: " << bulls << endl;
+    cout << "Cows: " << cows << endl;
+    return true;
+}
+
+void play_game(vector<int> secret, int cycle_counter) {
+    while(true) {
+        vector<int> guess = user_guess();
+        if(!game_cycle(guess, secret, cycle_counter)) {
+            cout << "Congrats!\nYou found all bulls.\nSecret number is: ";
+            print_vector_ints(secret);
+            cout << "\nAttempts: " << cycle_counter << endl;
+            break;
+        }
+        cycle_counter++;
+        cout << "___\n\n";
+    }
 }
 
 int main() {
-    int guess = user_guess();
-    int secret = secret_four_digit();
-    cout << secret << ':' << guess << endl;
+    vector<int> secret = secret_num();
+    int cycle_counter = 1;
+
+    // print_vector_ints(secret);   // Show SECRET number
+    cout << endl;
+
+    play_game(secret, cycle_counter);
   
     return 0;
 }
